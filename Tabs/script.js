a28540d4c75d72ad9e0f562447cadbe5b3198e76
Prajwalhn18 <prajwalhaniya18@gmail.com>
2023-08-14 00:24:51 +0530

    tabs

---
+++ Tabs/script.js
@@ -0,0 +1,16 @@
+function openTab(event, tab) {
+    console.log({ tab });
+    const tabcontents = document.getElementsByClassName('tabcontent');
+    const tablinks = document.getElementsByClassName('tablink');
+
+    for (let i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].style.display = "none";
+    }
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(' active', "");
+    }
+
+    document.getElementById(tab).style.display = "block";
+    event.currentTarget.className += " active";
+}
